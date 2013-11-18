65d60f5a66dba64067fa0f25cbe324706f08b2bc
Kristian Glass <git@doismellburning.co.uk>
2013-11-18 14:22:24 +0000

    Quick hacky "php"/HTML for Heroku app

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
+		<meta http-equiv="refresh" content="300">
+		<title>G4HSK QRSS (Part-time) Grabber</title>
+		<style>
+			body { text-align: center; }
+		</style>
+	</head>
+	<body>
+		<h1>G4HSK QRSS (Part-time) Grabber</h1>
+		<h2>Monitoring frequency shown in capture</h2>
+		<h3>(Please check status below to see if online.)</h3>
+		<br>
+		<br>
+		<img src="https://dl.dropboxusercontent.com/u/45053907/G4HSK_QRSS_Captures.jpg" width="1100" height="700" alt="Grabber Image">
+		<br>
+		<br>
+		<p>The capture updates every 5 minutes and the page will auto refresh</p>
+	</body>
+</html>
